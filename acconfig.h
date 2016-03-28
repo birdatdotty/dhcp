b07fb4cb78e2f1516f90d566d034fa36d86dc0c0
Anatoliy Mukha <bird@dotty.su>
2016-03-28 10:42:23 +0300

    step 2

--- acconfig.h
+++ acconfig.h
@@ -1,4 +1,11 @@
-#define NSUPDATE_FULL "/usr/bin/echo"
+/* Название пакета. */
+#undef PACKAGE
+
+/* Версия пакета.   */
+#undef VERSION
+
+@BOTTOM@
+#define NSUPDATE_FULL "/usr/bin/nsupdate"
 #define NSUPDATE "nsupdate"
 #define NSUPDATE_OPT "-g"
 #define NSUPDATE_END "send\n"
